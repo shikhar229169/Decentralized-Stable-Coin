28316940562a64d12c498da2e631dad61e4e71b3
shikhar229 <shikharagarwale7@gmail.com>
2023-07-17 21:57:00 +0530

    stale price check library added

---
+++ src/libraries/OracleLib.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+/**@title OracleLib
+ * @author Shikhar Agarwal
+ * @notice This library is used to check the chainlink oracle for stale data.
+ * If the price is stale, fundtion will revert, and render the DSCEngine unusable 
+ * DSCEngine will freeze if price becomes stale.
+*/
+library OracleLib {
+    error OracleLib__StalePrice();
+
+    uint256 private constant TIMEOUT = 3 hours;
+
+    function stalePriceCheck(AggregatorV3Interface priceFeed) public view returns (uint80, int256, uint256, uint256, uint80) {
+        (uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound) = priceFeed.latestRoundData();
+
+        uint256 secondsSince = block.timestamp - updatedAt;
+
+        if (secondsSince > TIMEOUT) {
+            revert OracleLib__StalePrice();
+        }
+
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
+    } 
+}
